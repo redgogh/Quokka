97c64b17589786333ee1decea5a944221d1a2e46
Red Gogh <redgogh@vip.qq.com>
2025-09-09 16:07:34 +0800

    移除 modules 包，将所有三方库移动到 thirdparty 文件夹下

---
+++ thirdparty/imgui/include/qk_imgui/qk_imgui.h
@@ -0,0 +1,31 @@
+#ifndef QK_IMGUI_H_
+#define QK_IMGUI_H_
+
+#include "imgui/imgui.h"
+#include "imgui/backends/imgui_impl_glfw.h"
+#include "imgui/backends/imgui_impl_vulkan.h"
+
+void QkImGuiVulkanHInit(GLFWwindow* window, ImGui_ImplVulkan_InitInfo* info);
+void QkImGuiVulkanHTerminate();
+
+void QkImGuiVulkanHNewFrame([[maybe_unused]] VkCommandBuffer commandBuffer);
+void QkImGuiVulkanHEndFrame(VkCommandBuffer commandBuffer);
+
+bool QkImGuiBegin(const char *title, bool* p_open = NULL, ImGuiWindowFlags flags = 0);
+void QkImGuiEnd();
+bool QkImGuiBeginViewport(const char *title);
+void QkImGuiEndViewport();
+
+// widgets
+bool QkImGuiDragFloat(const char *label, float *v, float v_speed = 1.0f, float v_min = 0.0f, float v_max = 0.0f, const char* format = "%.3f");
+bool QkImGuiDragFloat2(const char *label, float v[2], float v_speed = 1.0f, float v_min = 0.0f, float v_max = 0.0f, const char* format = "%.3f");
+bool QkImGuiDragFloat3(const char *label, float v[3], float v_speed = 1.0f, float v_min = 0.0f, float v_max = 0.0f, const char* format = "%.3f");
+bool QkImGuiDragFloat4(const char *label, float v[4], float v_speed = 1.0f, float v_min = 0.0f, float v_max = 0.0f, const char* format = "%.3f");
+void QkImGuiColorEdit3(const char* label, float col[3], ImGuiColorEditFlags flags = 0);
+void QkImGuiSliderFloat(const char* label, float* v, float v_min, float v_max, const char* format = "%.2f", ImGuiSliderFlags flags = 0);
+
+// vulkan
+ImTextureID QkImGuiAddTexture(VkSampler v_sampler, VkImageView v_image, VkImageLayout v_layout);
+void QkImGuiRemoveTexture(ImTextureID v_texture);
+
+#endif /* QK_IMGUI_H_ */
---
+++ thirdparty/imgui/qk_imgui.cpp
@@ -0,0 +1,358 @@
+#include "qk_imgui/qk_imgui.h"
+
+#include <GLFW//glfw3.h>
+#include <quokka/typedefs.h>
+#include <math.h>
+
+/*
+ * 可调节拖动平滑系数
+ *
+ * 在 ImGui DragFloat 函数隐藏鼠标拖动时，有时候会触发预期之外的 “飙数” 操作，在某一次拖动
+ * 可能导致数值增长或减小特别快，超出 v_speed 预期之外。
+ *
+ * 定义可调节拖动平滑系数可以有效解决 “飙数” 问题
+ */
+#define DRAG_SMOOTH_FACTOR 6.5f;
+
+static VkFormat* colorAttachmentFormats = nullptr;
+static GLFWwindow* g_Window = nullptr;
+
+void QkImGuiSetDarkNavUITheme()
+{
+    ImVec4* colors = ImGui::GetStyle().Colors;
+    colors[ImGuiCol_Text]                   = ImVec4(1.00f, 1.00f, 1.00f, 1.00f);
+    colors[ImGuiCol_TextDisabled]           = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
+    colors[ImGuiCol_WindowBg]               = ImVec4(0.10f, 0.10f, 0.10f, 1.00f);
+    colors[ImGuiCol_ChildBg]                = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+    colors[ImGuiCol_PopupBg]                = ImVec4(0.19f, 0.19f, 0.19f, 0.92f);
+    colors[ImGuiCol_Border]                 = ImVec4(0.19f, 0.19f, 0.19f, 0.29f);
+    colors[ImGuiCol_BorderShadow]           = ImVec4(0.00f, 0.00f, 0.00f, 0.24f);
+    colors[ImGuiCol_FrameBg]                = ImVec4(0.05f, 0.05f, 0.05f, 0.54f);
+    colors[ImGuiCol_FrameBgHovered]         = ImVec4(0.19f, 0.19f, 0.19f, 0.54f);
+    colors[ImGuiCol_FrameBgActive]          = ImVec4(0.20f, 0.22f, 0.23f, 1.00f);
+    colors[ImGuiCol_TitleBg]                = ImVec4(0.12f, 0.12f, 0.12f, 1.00f);
+    colors[ImGuiCol_TitleBgActive]          = ImVec4(0.06f, 0.06f, 0.06f, 1.00f);
+    colors[ImGuiCol_TitleBgCollapsed]       = ImVec4(0.00f, 0.00f, 0.00f, 1.00f);
+//  colors[ImGuiCol_MenuBarBg]              = ImVec4(0.14f, 0.14f, 0.14f, 1.00f);
+//  colors[ImGuiCol_MenuBarBg]              = ImVec4(0.12f, 0.12f, 0.12f, 1.00f);
+    colors[ImGuiCol_MenuBarBg]              = ImVec4(0.08f, 0.08f, 0.08f, 1.00f);
+    colors[ImGuiCol_ScrollbarBg]            = ImVec4(0.05f, 0.05f, 0.05f, 0.54f);
+//  colors[ImGuiCol_ScrollbarGrab]          = ImVec4(0.58f, 0.58f, 0.58f, 0.54f);
+    colors[ImGuiCol_ScrollbarGrab]          = ImVec4(0.00f, 0.46f, 1.00f, 0.54f);
+    colors[ImGuiCol_ScrollbarGrabHovered]   = ImVec4(0.40f, 0.40f, 0.40f, 0.54f);
+    colors[ImGuiCol_ScrollbarGrabActive]    = ImVec4(0.56f, 0.56f, 0.56f, 0.54f);
+    colors[ImGuiCol_CheckMark]              = ImVec4(0.33f, 0.67f, 0.86f, 1.00f);
+    colors[ImGuiCol_SliderGrab]             = ImVec4(0.34f, 0.34f, 0.34f, 0.54f);
+    colors[ImGuiCol_SliderGrabActive]       = ImVec4(0.56f, 0.56f, 0.56f, 0.54f);
+    colors[ImGuiCol_Button]                 = ImVec4(0.05f, 0.05f, 0.05f, 0.54f);
+    colors[ImGuiCol_ButtonHovered]          = ImVec4(0.19f, 0.19f, 0.19f, 0.54f);
+    colors[ImGuiCol_ButtonActive]           = ImVec4(0.20f, 0.22f, 0.23f, 1.00f);
+//  colors[ImGuiCol_Header]                 = ImVec4(0.00f, 0.00f, 0.00f, 0.52f);
+    colors[ImGuiCol_Header]                 = ImVec4(0.25f, 0.25f, 0.25f, 0.52f);
+    colors[ImGuiCol_HeaderHovered]          = ImVec4(0.00f, 0.00f, 0.00f, 0.36f);
+    colors[ImGuiCol_HeaderActive]           = ImVec4(0.20f, 0.22f, 0.23f, 0.33f);
+    colors[ImGuiCol_Separator]              = ImVec4(0.28f, 0.28f, 0.28f, 0.29f);
+    colors[ImGuiCol_SeparatorHovered]       = ImVec4(0.44f, 0.44f, 0.44f, 0.29f);
+    colors[ImGuiCol_SeparatorActive]        = ImVec4(0.40f, 0.44f, 0.47f, 1.00f);
+    colors[ImGuiCol_ResizeGrip]             = ImVec4(0.28f, 0.28f, 0.28f, 0.29f);
+    colors[ImGuiCol_ResizeGripHovered]      = ImVec4(0.44f, 0.44f, 0.44f, 0.29f);
+    colors[ImGuiCol_ResizeGripActive]       = ImVec4(0.40f, 0.44f, 0.47f, 1.00f);
+    colors[ImGuiCol_Tab]                    = ImVec4(0.00f, 0.00f, 0.00f, 0.52f);
+    colors[ImGuiCol_TabHovered]             = ImVec4(0.14f, 0.14f, 0.14f, 1.00f);
+    colors[ImGuiCol_TabActive]              = ImVec4(0.20f, 0.20f, 0.20f, 0.36f);
+    colors[ImGuiCol_TabUnfocused]           = ImVec4(0.00f, 0.00f, 0.00f, 0.52f);
+    colors[ImGuiCol_TabUnfocusedActive]     = ImVec4(0.14f, 0.14f, 0.14f, 1.00f);
+    colors[ImGuiCol_DockingPreview]         = ImVec4(0.33f, 0.67f, 0.86f, 1.00f);
+    colors[ImGuiCol_DockingEmptyBg]         = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+    colors[ImGuiCol_PlotLines]              = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
+    colors[ImGuiCol_PlotLinesHovered]       = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
+    colors[ImGuiCol_PlotHistogram]          = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
+    colors[ImGuiCol_PlotHistogramHovered]   = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
+    colors[ImGuiCol_TableHeaderBg]          = ImVec4(0.00f, 0.00f, 0.00f, 0.52f);
+    colors[ImGuiCol_TableBorderStrong]      = ImVec4(0.00f, 0.00f, 0.00f, 0.52f);
+    colors[ImGuiCol_TableBorderLight]       = ImVec4(0.28f, 0.28f, 0.28f, 0.29f);
+    colors[ImGuiCol_TableRowBg]             = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
+    colors[ImGuiCol_TableRowBgAlt]          = ImVec4(1.00f, 1.00f, 1.00f, 0.06f);
+    colors[ImGuiCol_TextSelectedBg]         = ImVec4(0.20f, 0.22f, 0.23f, 1.00f);
+    colors[ImGuiCol_DragDropTarget]         = ImVec4(0.33f, 0.67f, 0.86f, 1.00f);
+    colors[ImGuiCol_NavHighlight]           = ImVec4(1.00f, 0.00f, 0.00f, 1.00f);
+    colors[ImGuiCol_NavWindowingHighlight]  = ImVec4(1.00f, 0.00f, 0.00f, 0.70f);
+    colors[ImGuiCol_NavWindowingDimBg]      = ImVec4(1.00f, 0.00f, 0.00f, 0.20f);
+    colors[ImGuiCol_ModalWindowDimBg]       = ImVec4(1.00f, 0.00f, 0.00f, 0.35f);
+
+    ImGuiStyle& style = ImGui::GetStyle();
+    style.WindowPadding                     = ImVec2(8.00f, 8.00f);
+    style.FramePadding                      = ImVec2(5.00f, 2.00f);
+    style.CellPadding                       = ImVec2(6.00f, 6.00f);
+    style.ItemSpacing                       = ImVec2(6.00f, 6.00f);
+    style.ItemInnerSpacing                  = ImVec2(6.00f, 6.00f);
+    style.TouchExtraPadding                 = ImVec2(0.00f, 0.00f);
+    style.IndentSpacing                     = 25;
+    style.ScrollbarSize                     = 15;
+    style.GrabMinSize                       = 16;
+    style.WindowBorderSize                  = 1;
+    style.ChildBorderSize                   = 1;
+    style.PopupBorderSize                   = 1;
+    style.FrameBorderSize                   = 0;
+    style.TabBorderSize                     = 1;
+    style.WindowRounding                    = 7;
+    style.ChildRounding                     = 4;
+    style.FrameRounding                     = 3;
+    style.PopupRounding                     = 12;
+    style.ScrollbarRounding                 = 9;
+    style.GrabRounding                      = 3;
+    style.LogSliderDeadzone                 = 4;
+    style.TabRounding                       = 4;
+}
+
+void _CheckDraggingCursor()
+{
+    static bool     is_dragging = false;
+    static ImGuiID  current_drag_item = 0x7FFFFFFF;
+
+    ImGuiID item = ImGui::GetItemID();
+    bool check_active = ImGui::IsItemActive() && ImGui::IsMouseDragging(0);
+
+    if (item == current_drag_item && !check_active) {
+        is_dragging = false;
+        glfwSetInputMode(g_Window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        current_drag_item = 0x7FFFFFFF;
+        return;
+    }
+
+    if (!is_dragging && check_active) {
+        is_dragging = true;
+        current_drag_item = item;
+        glfwSetInputMode(g_Window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+        return;
+    }
+}
+
+bool _DragScalarN(const char *label, float *v, int v_number, float v_speed, float v_min, float v_max, const char *format)
+{
+    assert(v_number <= 4);
+
+    bool trigger = false;
+
+    float watch[v_number];
+
+    for (int i = 0; i < v_number; i++)
+        watch[i] = v[i];
+
+    ImGui::BeginGroup();
+    ImGui::PushID(label);
+    ImGui::Indent(32.0f);
+    ImGui::TextUnformatted(label);
+    ImGui::SameLine();
+
+    static const char *axis_labels[4] = { "X", "Y", "Z", "W" };
+    static constexpr ImVec4 axis_colors[4] = {
+        ImVec4(1.0f, 0.0f, 0.0f, 1.0f), // R
+        ImVec4(0.0f, 1.0f, 0.0f, 1.0f), // G
+        ImVec4(0.0f, 0.4f, 1.0f, 1.0f), // B
+        ImVec4(1.0f, 1.0f, 1.0f, 1.0f), // W
+    };
+
+    if (v_number > 1) {
+        ImVec2 region = ImGui::GetContentRegionAvail();
+        float single_item_width = region.x * 0.65f / 3;
+        for (int i = 0; i < v_number; i++) {
+            const char *id = axis_labels[i];
+            ImGui::PushID(id);
+            ImGui::PushItemWidth(single_item_width);
+            ImGui::TextColored(axis_colors[i], "%s", axis_labels[i]);
+            ImGui::SameLine();
+            if (ImGui::DragFloat("", &v[i], v_speed, v_min, v_max, format)) {
+                trigger = true;
+
+                float delta = v[i] - watch[i];
+                float max_delta = v_speed * DRAG_SMOOTH_FACTOR;
+
+                if (fabs(delta) > max_delta)
+                    v[i] = watch[i] + (delta > 0 ? max_delta : -max_delta);
+            }
+            _CheckDraggingCursor();
+            ImGui::SameLine();
+            ImGui::PopItemWidth();
+            ImGui::PopID();
+        }
+        goto DRAG_SCALAR_N_END_TAG;
+    }
+
+    // if v_number < 1
+    if (ImGui::DragFloat("", v, v_speed, v_min, v_max, format))
+        trigger = true;
+
+    _CheckDraggingCursor();
+
+DRAG_SCALAR_N_END_TAG:
+    ImGui::Unindent(32.0f);
+    ImGui::PopID();
+    ImGui::EndGroup();
+
+    return trigger;
+}
+
+void QkImGuiVulkanHInit(GLFWwindow* window, ImGui_ImplVulkan_InitInfo* info)
+{
+    g_Window = window;
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;         // Enable Docking
+    io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;       // Enable Multi-Viewport / Platform Windows
+    io.ConfigViewportsNoAutoMerge = true;
+    io.ConfigViewportsNoTaskBarIcon = true;
+
+    // Set ImGui default font.
+    io.Fonts->AddFontFromFileTTF("../misc/fonts/Microsoft Yahei UI/Microsoft Yahei UI.ttf", 18.0f, nullptr, io.Fonts->GetGlyphRangesChineseFull());
+    io.FontDefault = io.Fonts->Fonts.back();
+
+    QkImGuiSetDarkNavUITheme();
+
+    // When viewports are enabled we tweak WindowRounding/WindowBg so platform windows can look identical to regular ones.
+    ImGuiStyle& style = ImGui::GetStyle();
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+    {
+        style.WindowRounding = 0.0f;
+        style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+    }
+
+    /*
+     * 为 colorAttachmentFormats 分配堆内存，避免出现内存地址不匹配问题。
+     *
+     * 因为 ImGui 的 Pipeline 创建是懒加载的，如果外面的函数被释放了这里可能会导致数组指针
+     * 的 format 格式异常。所以为了确保在 ImGui_ImplVulkan_RenderDrawData(...) 函数的
+     * 正常调用以及正常创建管线，在 qk_imgui 内部自行管理 colorAttachmentFormats。
+     */
+    const uint32_t count = info->PipelineRenderingCreateInfo.colorAttachmentCount;
+    colorAttachmentFormats = new VkFormat[count];
+
+    for (uint32_t i = 0; i < count; i++)
+        colorAttachmentFormats[i] = info->PipelineRenderingCreateInfo.pColorAttachmentFormats[i];
+
+    info->PipelineRenderingCreateInfo.pColorAttachmentFormats = colorAttachmentFormats;
+
+    ImGui_ImplGlfw_InitForVulkan(window, true);
+    ImGui_ImplVulkan_Init(info);
+}
+
+void QkImGuiVulkanHTerminate()
+{
+    delete[] colorAttachmentFormats;
+    ImGui_ImplGlfw_Shutdown();
+    ImGui_ImplVulkan_Shutdown();
+}
+
+void QkImGuiVulkanHNewFrame(QK_MAYBE_UNUSED VkCommandBuffer commandBuffer)
+{
+    ImGui_ImplVulkan_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    // docking
+    ImGui::DockSpaceOverViewport();
+}
+
+void QkImGuiVulkanHEndFrame(VkCommandBuffer commandBuffer)
+{
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+
+    // Rendering
+    ImGui::Render();
+    ImDrawData* main_draw_data = ImGui::GetDrawData();
+    ImGui_ImplVulkan_RenderDrawData(main_draw_data, commandBuffer);
+
+    // Update and Render additional Platform Windows
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+        ImGui::UpdatePlatformWindows();
+        ImGui::RenderPlatformWindowsDefault();
+    }
+}
+
+bool QkImGuiBegin(const char *title, bool* p_open, ImGuiWindowFlags flags)
+{
+    bool retval;
+    ImGuiStyle &style = ImGui::GetStyle();
+    ImVec2 padding = style.FramePadding;
+    padding.y = padding.y + 5;
+    ImGui::PushStyleVar(ImGuiStyleVar_FramePadding, padding);
+    retval = ImGui::Begin(title, p_open, flags);
+    ImGui::PopStyleVar();
+    return retval;
+}
+
+void QkImGuiEnd()
+{
+    ImGui::End();
+}
+
+bool QkImGuiBeginViewport(const char *title)
+{
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0, 0));
+    ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0);
+    return ImGui::Begin(title);
+}
+
+void QkImGuiEndViewport()
+{
+    ImGui::End();
+    ImGui::PopStyleVar(2);
+}
+
+bool QkImGuiDragFloat(const char *label, float *v, float v_speed, float v_min, float v_max, const char *format)
+{
+    return _DragScalarN(label, v, 1, v_speed, v_min, v_max, format);
+}
+
+bool QkImGuiDragFloat2(const char *label, float *v, float v_speed, float v_min, float v_max, const char *format)
+{
+    return _DragScalarN(label, v, 2, v_speed, v_min, v_max, format);
+}
+
+bool QkImGuiDragFloat3(const char *label, float *v, float v_speed, float v_min, float v_max, const char *format)
+{
+    return _DragScalarN(label, v, 3, v_speed, v_min, v_max, format);
+}
+
+bool QkImGuiDragFloat4(const char *label, float *v, float v_speed, float v_min, float v_max, const char *format)
+{
+    return _DragScalarN(label, v, 4, v_speed, v_min, v_max, format);
+}
+
+void QkImGuiColorEdit3(const char* label, float *col, ImGuiColorEditFlags flags)
+{
+    ImGui::PushID(label);
+    ImGui::Indent(32.0f);
+    ImGui::TextUnformatted(label);
+    ImGui::SameLine();
+    ImGui::ColorEdit3("", col, flags);
+    ImGui::Unindent(32.0f);
+    ImGui::PopID();
+}
+
+void QkImGuiSliderFloat(const char* label, float* v, float v_min, float v_max, const char* format, ImGuiSliderFlags flags)
+{
+    ImGui::PushID(label);
+    ImGui::Indent(32.0f);
+    ImGui::TextUnformatted(label);
+    ImGui::SameLine();
+    ImGui::SliderFloat("", v, v_min, v_max, format, flags);
+    ImGui::Unindent(32.0f);
+    ImGui::PopID();
+}
+
+ImTextureID QkImGuiAddTexture(VkSampler v_sampler, VkImageView v_image, VkImageLayout v_layout)
+{
+    return ImGui_ImplVulkan_AddTexture(v_sampler, v_image, v_layout);
+}
+
+void QkImGuiRemoveTexture(ImTextureID v_texture)
+{
+    ImGui_ImplVulkan_RemoveTexture((VkDescriptorSet) v_texture);
+}
